820030fd37d9aba8654c50cb0eb13f78a974adc8
ghostchinchilla <133160254+ghostchinchilla@users.noreply.github.com>
2023-07-05 11:19:51 -0400

    Add files via upload

---
+++ letAndConst.js
@@ -0,0 +1,14 @@
+var PI = 3.14;
+PI = 42;
+
+const PI = 3.14;
+PI = 42;
+
+//var can be redeclared and reassigned at any point. let can be reassigned, but 
+//cannot be redeclared in the same scope.
+
+//you can redeclare and reassign with var, but const can only be mutated(ex: change contects of array)
+
+//you can only reassign let, not redeclare. you cannot redeclare or reassign const.
+
+//hoisting is when variables are used before they are declared.
